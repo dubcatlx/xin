9647c0600d72d0f9c80b2db0f2de7749b65b7b7c
dubcalx <dubcat@sina.com>
2020-03-20 12:04:44 +0800

    123

--- test/test.c
+++ test/test.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-	/* update xin by code 1 test*/
+	/* update xin by code 1111 test*/
 	printf("test commit");
 	printf("123");
 	printf("%s\n", "hello world");
