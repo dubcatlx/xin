f16b978a24b1c7b007eda0a2949df5e2010ad47c
李新 <dubcat@sina.com>
2020-03-20 12:05:35 +0800

    update

--- test/test.c
+++ test/test.c
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
 {
 	/* update xin by code 1111 test*/
 	printf("test commit");
-	printf("123");
+	printf("123456");
 	printf("%s\n", "hello world");
 	return 0;
 }
